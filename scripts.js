9074d657b3bb45b660d0a2768af56a04d13b5a5e
nperez3-droid <n_perez3@mail.fhsu.edu>
2025-08-23 12:25:50 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-/* Place your JavaScript in this file */
+// Get references to elements
+let myHeading = document.querySelector("h1");
+let myButton = document.querySelector("button");
+
+// Change heading text when button is clicked
+myButton.onclick = function () {
+  myHeading.textContent = "Hello world!";
+};
